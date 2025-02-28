105d8dce4b87d6a0bfd8ab50dbbf7d7db099c15b
BlushyTears <41865946+BlushyTears@users.noreply.github.com>
2025-02-28 13:24:50 +0100

    Update DBST.h

--- DBST.h
+++ DBST.h
@@ -1,23 +1,68 @@
 #pragma once
 
-struct DBST
-{
-	int value;
-	int c;
-	DBST *parent;
-	DBST *leftNode;
-	DBST *rightNode;
+#include <iostream>
+#include <vector>
 
-	DBST(int data, DBST* parent, int c)
-	{
-		this->value = data;
-		this->c = c;
-		this->leftNode = nullptr;
-		this->rightNode = nullptr;
-		this->parent = parent;
-	}
+using namespace std;
 
-	void Insert(int ins);
-	void Rearrange();
-	int NodeSize();
+template <typename T>
+struct GBSTTreeNode {
+    T data;
+    GBSTTreeNode* left;
+    GBSTTreeNode* right;
+
+    GBSTTreeNode(T _data) {
+        data = _data;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+
+//template <typename T>
+//static GBSTTreeNode<int>* gbtn = nullptr;
+struct GBST {
+    //total number of nodes, includes root node
+    template <typename T>
+    int getSubtreeSize(GBSTTreeNode<T>* root) {
+        if (root == nullptr)
+            return 0;
+        return getSubtreeSize(root->left) + getSubtreeSize(root->right) + 1;
+    }
+
+    template <typename T>
+    GBSTTreeNode<T>* createNode(T _data) {
+        return new GBSTTreeNode<T>(_data);
+    }
+
+    template <typename T>
+    GBSTTreeNode<T>* insertNode(GBSTTreeNode<T>* _root, T _data) {
+        // base case:
+        if (_root == nullptr) {
+            return createNode(_data);
+        }
+
+        // Special rule: Duplicates are treated as less than than _root, and therefore put as left node
+        if (_data <= _root->data) {
+            _root->left = insertNode(_root->left, _data);
+        }
+        else if (_data > _root->data) {
+            _root->right = insertNode(_root->right, _data);
+        }
+
+        return _root;
+    }
+
+    template <typename T>
+    void traverseInOrder(GBSTTreeNode<T>* _root, int level = 0) {
+        if (_root != nullptr) {
+            cout << "Level: " << level << " |";
+            cout << getSubtreeSize(_root->left) << " <- left subtree size || right subtree size -> " << getSubtreeSize(_root->right) << endl;
+
+            traverseInOrder(_root->left, level + 1);
+            // Message To Grette: to print tree in-order: Uncomment this
+            //cout << _root->data << " ";
+            traverseInOrder(_root->right, level + 1);
+        }
+    }
 };
